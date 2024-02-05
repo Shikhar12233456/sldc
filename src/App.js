d912589f434aeb482476fb8dbfe378e56d87e8e9
adarshk1410 <87784034+adarshk1410@users.noreply.github.com>
2024-02-05 14:21:57 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import Home from "./components/home";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Homeome";
+import Dayahead from "./components/Dayahead"
+
 function App() {
   return (
+    <>
     <div className={"w-full h-full"}>
-      <Home/>
+     
+       <Routes>
+        <Route path="/" element={<Home />} />      
+        <Route path="/Dayahead" element={<Dayahead />} />
+       </Routes>
+      
     </div>
+   </>
   );
 }
-
 export default App;
