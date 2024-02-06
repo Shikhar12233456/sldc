d2a5f804e07322b5a0a2861f1aada8f116fbdf92
Samuel Masih <samuelmasih.sls777@gmail.com>
2024-02-07 03:56:27 +0530

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-
+import Navbar from "./Components/Navbar"
 import Header from "../src/Components/Header"
-import Graph from "./Components/Graph";
+import Chart from "react-apexcharts";
 function App() {
+  const state = {
+    options: {        
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+        }
+      },
+    chart: {
+        type: 'area'
+    },
+    stroke: {
+        curve: 'smooth',
+      },
+    series: [
+        {
+         type:'area',
+          name: "series-1",
+          data: [30, 40, 45, 50, 49, 60, 70, 91]
+        }
+      ]
+  };
   return (
     <div >
       {/* <Header/> */}
-      <Graph/>
+      {/* <Graph/> */}
+      <Navbar/>
+      <Header/>
     </div>
   );
 }
