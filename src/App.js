b9017f88208f5a6869e35c8f2635cb892e04bb3e
adarshk1410 <87784034+adarshk1410@users.noreply.github.com>
2024-02-06 18:25:02 +0530

    Update App.js
    FIxed Home component import from Homeome to home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./components/Homeome";
+import Home from "./components/home";
 import Dayahead from "./components/Dayahead"
 
 function App() {
